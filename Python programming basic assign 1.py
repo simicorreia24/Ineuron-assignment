d363f5cfe10b9b5f5670a49e2e8228cfce7806b0
Simi Silvester Correia <66421852+simicorreia24@users.noreply.github.com>
2021-05-27 16:41:04 +0530

    Add files via upload

---
+++ Python programming basic assign 1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Write a Python program to print "Hello Python"
+
+# In[15]:
+
+
+print("Hello Python")
+
+
+# 2. Write a Python program to do arithmetical operations addition and division.?
+
+# In[16]:
+
+
+addition = 10+15
+division = 20/10
+print(addition)
+print(division)
+
+
+# 3. Write a Python program to find the area of a triangle?
+
+# In[3]:
+
+
+b= int(input("Enter the base of triangle "))
+h= int(input("Enter the height of triangle "))
+area_of_a_triangle =(b * h)/2
+area_of_a_triangle
+
+
+# 4. Write a Python program to swap two variables?
+
+# In[12]:
+
+
+a= int(input("Enter the value of a "))
+b= int(input("Enter the value of b "))
+c=b
+b=a
+a=c
+print("The swapped value of a = ",a)
+print("The swapped value of b = ",b)
+
+
+# 5. Write a Python program to generate a random number?
+
+# In[5]:
+
+
+import random
+a = random.randint(0,10)
+a
+
